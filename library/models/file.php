9a391e8b71239df1695012c69cde0b13e1773759
David Boskovic <david@momentumapp.co>
2012-09-14 14:20:04 -0600

    copy functionality

--- library/models/file.php
+++ library/models/file.php
@@ -6,7 +6,92 @@ use Exception;
 use e;
 
 class File extends Model {
-	
+
+	/**
+	 * Handle the file upload and save the file to the proper location
+	 * @author: David Boskovic
+	 */
+	public function move($file, $owner, $ext, $type) {
+
+		/**
+		 * Create the Filename from the Owner map and a random hash
+		 */
+		$extension = '.'.$ext;
+		$filename = str_replace(':','_',$owner->__map()).'_'.md5(time().rand(10,99)).$extension;
+
+		/**
+		 * Use an external file storage bundle
+		 */
+		$eventResponse = e::$events->uploadFile($file, $filename);
+
+		/**
+		 * Use built in local file storage
+		 */
+		if(empty($eventResponse)) {
+
+			/**
+			 * Get the upload location
+			 */
+			$upload_dir = e::$resource->requireDir();
+
+			/**
+			 * Append the file upload location
+			 */
+			$filename = $upload_dir.'/'.$filename;
+
+			/**
+			 * Is the upload directory writable
+			 */
+			if(!is_writable(dirname($filename)))
+				throw new Exception('Make upload directory writable: `dir=' . dirname($filename) . ';mkdir -p $dir;chmod 0777 $dir`');
+
+			/**
+			 * Move the uploaded file & check for any file upload errors
+			 */
+			if(!rename($file, $filename)) throw new Exception("Uploaded file `".$file."` could not be moved.");
+		}
+
+		/**
+		 * Get the response from the external file storage bundle
+		 */
+		else foreach($eventResponse as $event) {
+
+			/**
+			 * If the bundle did not handle the upload
+			 */
+			if(empty($event)) continue;
+
+			/**
+			 * Get the filename returned by the bundle
+			 */
+			if(!isset($_filename)) $_filename = $event['filename'];
+			else $_filename .= ':'.$_filename;
+		}
+
+		/**
+		 * If the event response created the filenames
+		 */
+		if(isset($_filename)) $filename = $_filename;
+
+		/**
+		 * Save the model with the file information
+		 */
+		$this->filename = $filename;
+		$this->hash = md5($filename);
+		$this->type = $type;
+		$this->save();
+
+		/**
+		 * Tag the owner
+		 */
+		$this->_->taxonomy->addTag($owner);
+
+		/**
+		 * Return the model
+		 */
+		return $this;
+	}
+
 	/**
 	 * Handle the file upload and save the file to the proper location
 	 * @author: Kelly Lauren Summer Becker
